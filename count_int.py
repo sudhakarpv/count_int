85f5259d311c55f75fe4d5d19f30177c4183c72c
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-22 10:29:58 +0530

    Update count_int.py

--- count_int.py
+++ count_int.py
@@ -1,3 +1,12 @@
 # count_int
-user=list(map(int,input().split()))
-print(len(user))
+def main():
+    pass
+u=input()
+l=list(u)
+count=0
+for i in range(len(l)):
+    count=+i
+print(count+1)
+
+if __name__ == '__main__':
+    main()
